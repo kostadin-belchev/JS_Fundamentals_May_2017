0360523183adad0b9d3b55f15147ad164ad48460
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-01-15 19:22:55 +0200

    coorected 5. String of Numbers 1..N task

--- 01_JS-Fundamentals-Intro-to-JavaScript-Lab/05_String of Numbers 1..N.js
+++ 01_JS-Fundamentals-Intro-to-JavaScript-Lab/05_String of Numbers 1..N.js
@@ -5,11 +5,4 @@ function stringOfNums (n) {
         stringOfNums += i.toString();
     }
     return stringOfNums;
-}function stringOfNums (n) {
-    let num = Number(n);
-    let stringOfNums = '';
-    for (let i = 1; i <= num; i++) {
-        stringOfNums += i.toString();
-    }
-    return stringOfNums;
 }
