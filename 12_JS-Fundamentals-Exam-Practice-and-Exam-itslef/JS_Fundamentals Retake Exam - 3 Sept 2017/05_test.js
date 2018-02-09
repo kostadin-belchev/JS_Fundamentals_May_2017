db9489f67274980217517325f63c066efaa1219a
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-02-09 22:49:03 +0200

    did the last exam task 1 and completed the last task from the second to last exam

--- 12_JS-Fundamentals-Exam-Practice-and-Exam-itslef/JS_Fundamentals Retake Exam - 3 Sept 2017/05_test.js
+++
@@ -1,9 +0,0 @@
-function repeatSignNTimes (sign = '', times) {
-    let string = '';
-    for (let i = 0; i < times; i++) {
-        string += sign;
-    }
-    return string;
-}
-
-console.log(repeatSignNTimes('*', 5));
